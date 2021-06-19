fdc845e2ac02f3cfb05780308c18ce6de2900587
Saran <sarantharma7@gmail.com>
2021-06-18 23:20:06 -0400

    Remove node modules

--- Point_Of_Sale/app.js
+++ Point_Of_Sale/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
+const Joi = require("Joi");
+const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
+
 const methodOverride = require("method-override");
 const Product = require("./models/product");
 // console.log(Product.findById("60c141f137faac103a5f60a5"));
@@ -33,41 +37,86 @@ app.get("/", function (req, res) {
   res.render("home");
 });
 
-app.get("/products", async function (req, res) {
-  const products = await Product.find({});
-  res.render("products/index", { products });
-});
+app.get(
+  "/products",
+  catchAsync(async (req, res) => {
+    const products = await Product.find({});
+    res.render("products/index", { products });
+  })
+);
 
 app.get("/products/new", (req, res) => {
   res.render("products/new");
 });
 
-app.post("/products", async (req, res) => {
-  const product = new Product(req.body.product);
-  await product.save();
-  res.redirect(`/products/${product._id}`);
+app.post(
+  "/products",
+  catchAsync(async (req, res) => {
+    // console.log(req.body.product);
+    const productSchema = Joi.object({
+      product: Joi.object({
+        name: Joi.string().required(),
+        unitprice: Joi.number().required().min(0),
+        // description: Joi.string().required(),
+        // quantity: Joi.number().required().min(0),
+        // image: String,
+      }).required(),
+    });
+    const { error } = productSchema.validate(req.body);
+    if (error) {
+      const msg = error.details.map((el) => el.message).join(",");
+      throw new ExpressError(msg, 400);
+    }
+    const product = new Product(req.body.product);
+    await product.save();
+    res.redirect(`/products/${product._id}`);
+  })
+);
+
+app.get(
+  "/products/:id",
+  catchAsync(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    res.render("products/show", { product });
+  })
+);
+
+app.get(
+  "/products/:id/edit",
+  catchAsync(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    res.render("products/edit", { product });
+  })
+);
+
+app.put(
+  "/products/:id",
+  catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, {
+      ...req.body.product,
+    });
+    res.redirect(`/products/${product._id}`);
+  })
+);
+
+app.delete(
+  "/products/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Product.findByIdAndDelete(id);
+    res.redirect("/products");
+  })
+);
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page not found", 404));
 });
 
-app.get("/products/:id", async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  res.render("products/show", { product });
-});
-
-app.get("/products/:id/edit", async (req, res) => {
-  const product = await Product.findById(req.params.id);
-  res.render("products/edit", { product });
-});
-
-app.put("/products/:id", async (req, res) => {
-  const { id } = req.params;
-  const product = await Product.findByIdAndUpdate(id, { ...req.body.product });
-  res.redirect(`/products/${product._id}`);
-});
-
-app.delete("/products/:id", async (req, res) => {
-  const { id } = req.params;
-  await Product.findByIdAndDelete(id);
-  res.redirect("/products");
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Something went wrong";
+  res.status(statusCode).render("error", { err });
 });
 
 app.listen(3000, () => {
